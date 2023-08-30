630f567b3b580607aa1a2413b1ad371832f61508
kalyan_matta <kalyan.matta@rajasoftwarelabs.com>
2023-08-30 16:26:18 +0530

    Added Textify button and dummy text

--- app/src/main/java/com/example/imagetotextapp/MainActivity.kt
+++ app/src/main/java/com/example/imagetotextapp/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.imagetotextapp
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -18,8 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     private val REQUEST_CODE = 123
 
-    private var txt: TextView? = null
+    private lateinit var txt: TextView
     private lateinit var img: ImageView
+    private lateinit var translateBtn: Button
 
     //For using Camera
     private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         txt = findViewById(R.id.textValue)
         img = findViewById(R.id.capturedImg)
+        translateBtn = findViewById(R.id.translate_btn)
 
         //Check Camara permissions provided. If not provided request for permissions
         if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -44,15 +46,27 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 val bitMap = result.data?.extras?.get("data") as Bitmap
                 //ste image in image view
+                translateBtn.visibility = View.VISIBLE
                 img.setImageBitmap(bitMap)
+
             }
         }
 
+        //Capture image
         findViewById<Button>(R.id.cameraBtn).setOnClickListener{
+            img.visibility = View.VISIBLE
+            txt.visibility = View.GONE
             val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             if (pictureIntent.resolveActivity(packageManager) != null) {
                 cameraLauncher.launch(pictureIntent)
             }
+        }
+
+        //Textify
+        translateBtn.setOnClickListener{
+            img.visibility = View.GONE
+            txt.visibility = View.VISIBLE
+            txt.text = "Translation completed abcdefghijklmnopqrstuvwxyz."
         }
     }
 }
